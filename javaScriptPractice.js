790878522c7ed7ba3fbcb15896f717cd94fdad4f
Kylania Saeteurn <76223982+Ilikesushi37@users.noreply.github.com>
2025-07-07 13:33:22 -0700

    This is th4 Java Script practice assignment

---
+++ javaScriptPractice.js
@@ -0,0 +1,20 @@
+
+const storeItems = {
+    shampoo : 10,
+    conditioner : 12,
+    bodyWash : 10,
+    scrub : 10,
+    cleanser : 15,
+    moisurizer : 20,
+    eyeCream : 50,
+    hairbrush : 8,
+    comb : 7,
+};
+
+const shoppingCart = ['cleanser', 'moisurizer', 'hairbrush', 'scrub'];
+
+//function shoppingCartCost () {
+
+//};
+
+console.log(shoppingCart[0]);
