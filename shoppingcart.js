4dacd8575d636373de9fa72d8668cf1bed077871
Kylania Saeteurn <76223982+Ilikesushi37@users.noreply.github.com>
2025-07-14 17:55:26 -0700

    I hate everything, nothing works so im taking a break

--- shoppingcart.js
+++ shoppingcart.js
@@ -3,10 +3,20 @@ const storeItems = {
     gatorade : 5,
     water : 1,
 };
-const buttonList = document.getElementsByTagName('button');
-for (const btn of buttonList) {
-    console.log('im in the for loop');
+
+
+// WHY WON'T THIS WORK?!!!
+//this inputs the text nodes from the button tag and creates a list :)
+const buttons = document.getElementsByTagName('button');
+const cart = document.getElementById("cart")
+
+
+
+for (const btn of buttons) {
+console.log('im in the for loop');
     // listen for click events
     // call the changeBackground() function when the event is triggered
     // note that the event handler argument is an anonymous function that accepts the event object as a parameter
-    btn.addEventListener('click', (event) => changeBackground(event, event.target.textContent));
+    btn.addEventListener('click', cartElement.classList.add(btn))
+};
+console.log("please work")
