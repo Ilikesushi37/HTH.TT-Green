24f86f3e0e062dd7811d25952c8d56ecf0480a82
Kylania Saeteurn <76223982+Ilikesushi37@users.noreply.github.com>
2025-07-21 16:51:59 -0700

    i put down some ideas at creating the buttons...but like Im paranoid that my stuff is gonna like crash out like me and delete itself so let me save everything before that happens

--- shoppingcart.js
+++ shoppingcart.js
@@ -3,11 +3,26 @@ const storeItems = {
     gatorade : 6,
     water : 2,
 };
+const jsStoreItem = document.getElementById('store-items');
 
-let jsStoreItems = document.getElementById('store-items'); // I want to take these 
+// this works but like it feels so inconvienient... like I think I can find a simpler solution 
+//for (const key in storeItems) {
+//     if (storeItems.hasOwnProperty(key)){ //this gets all the properties (no it doesn't)
+//             let newTextNode = document.createTextNode(key + ' ' );
+//            document.body.appendChild(newTextNode); 
+//   }
+//    };
+// I want to add this to the store-items id...
 
-for (const key in storeItems) {
-        jsStoreItems.classList.add(key);
+ // getting element by ID
+
+
+//right now this strategy relies on 
+function storeItemsInList (anObject) {
+    for (const key in storeItems) {
+        if (storeItems.hasOwnProperty(key)){ //this gets all the properties (no it doesn't)
+            storesSelectedItems =+ key;
+            };
     };
-
-console.log(jsStoreItems)
+};
+jsStoreItem.textContent =+ storeItemsInList (storeItems);
