dd62ef06f56a63515ba4aab519de6d1fe04c6f6f
Kylania Saeteurn <76223982+Ilikesushi37@users.noreply.github.com>
2025-07-21 19:47:05 -0700

    I found a solution to creating buttons out of iterating an object

--- shoppingcart.js
+++ shoppingcart.js
@@ -1,28 +1,22 @@
+
 const storeItems = {
     soda : 5,
     gatorade : 6,
     water : 2,
 };
+
+
+//translating the store-items id to be used in js
 const jsStoreItem = document.getElementById('store-items');
 
-// this works but like it feels so inconvienient... like I think I can find a simpler solution 
-//for (const key in storeItems) {
-//     if (storeItems.hasOwnProperty(key)){ //this gets all the properties (no it doesn't)
-//             let newTextNode = document.createTextNode(key + ' ' );
-//            document.body.appendChild(newTextNode); 
-//   }
-//    };
-// I want to add this to the store-items id...
+//This really confusing confunction takes all the keys of my storeItems object and turns them into button with button.textcontent
+Object.keys(storeItems).forEach(key => { //iterates the keys
+    const button = document.createElement('button'); //
+    button.textContent = key; 
 
- // getting element by ID
-
-
-//right now this strategy relies on 
-function storeItemsInList (anObject) {
-    for (const key in storeItems) {
-        if (storeItems.hasOwnProperty(key)){ //this gets all the properties (no it doesn't)
-            storesSelectedItems =+ key;
-            };
-    };
-};
-jsStoreItem.textContent =+ storeItemsInList (storeItems);
+    const cartsTotal = document.getElementById('cart-total')
+    button.addEventListener('click', () => {
+       cartsTotal =+ (storeItems[key]);
+    });
+    jsStoreItem.appendChild(button);
+});
