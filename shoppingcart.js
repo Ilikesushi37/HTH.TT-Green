3ae9e2f3ac2d295eddadf11f93d5895cae6e4a62
Kylania Saeteurn <76223982+Ilikesushi37@users.noreply.github.com>
2025-07-21 22:31:37 -0700

    more effective strategy for the button function :)

--- shoppingcart.js
+++ shoppingcart.js
@@ -9,14 +9,20 @@ const storeItems = {
 //translating the store-items id to be used in js
 const jsStoreItem = document.getElementById('store-items');
 
-//This really confusing confunction takes all the keys of my storeItems object and turns them into button with button.textcontent
-Object.keys(storeItems).forEach(key => { //iterates the keys
-    const button = document.createElement('button'); //
-    button.textContent = key; 
+//This really confusing but this function takes all the properties of my storeItems object and turns them into buttons with button.textcontent
+Object.keys(storeItems).forEach(key => { //iterates the props
+    const button = document.createElement('button'); //CREATES a new id called 'button' which matches the css name
+    button.textContent = key; //change the name of the new button made with the current index
 
-    const cartsTotal = document.getElementById('cart-total')
-    button.addEventListener('click', () => {
-       cartsTotal =+ (storeItems[key]);
+    let cartsTotal = document.getElementById('cart-total') //grab or translate the cart-total html id and turn it into js
+    button.addEventListener('click', function () { //on event that a button is clicked, do something!
+       for (const key in storeItems) { //iterate the storeitems object again!
+            if (Object.prototype.hasOwnProperty.call(storeItems, key)) { 
+                cartsTotal += storeItems[key];
+
+  };
+}
     });
     jsStoreItem.appendChild(button);
 });
+
